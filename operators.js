d9a05740086ead122922abe4126a475b6faccdb2
Berryzr <36480057+Berryzr@users.noreply.github.com>
2018-03-22 20:19:56 -0400

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,19 @@
+var x = 7;
+var y = "7";
+var z = 2;
+
+// equality
+x == y // x "equals" y; true
+x != y // x "not equal to" y; false
+
+// strict equality 
+x === y // x has the same data type and value as y; false
+x !== y // x has either a different value OR data type as y; true
+
+
+x < y  // false
+x < z  // false
+x <= y // true
+x <= z // false
+x > y  // false
+x >= y // true
