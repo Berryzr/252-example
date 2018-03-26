f00ee50452bf6f2d70763ab9772cb4ad88832e82
Berryzr <36480057+Berryzr@users.noreply.github.com>
2018-03-26 19:29:25 -0400

    Add files via upload

---
+++ collectors.js
@@ -0,0 +1,11 @@
+//Identifying relevent information of a Roommate
+let Roommate = {
+	"first_name": "Ethan",
+	"last_name": "Ratke",
+	"DoB": "1997-02-11",
+	"Sports": "Plays football"
+	"Football_Position":"Starting backup kicker",
+	"Favorite_food": "Dominos",
+	"Hobbies": "eating nothing but pizza and watching movies"
+};
+console.log(Roommate["Favorite_food"]); // outputs Dominos
